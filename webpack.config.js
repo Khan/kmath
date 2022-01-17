5ebb4df78203b577c57e94b9f48e08613182c47d
Jeremy Wiebe <jeremy@khanacademy.org>
2022-01-17 13:37:14 -0800

    Upgrade webpack and set up build now that we're using Flow

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: './index.js',
+    entry: "./index.js",
     output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'kmath.js',
-        library: 'kmath',
-        libraryTarget: 'umd',
+        path: path.join(__dirname, "build"),
+        filename: "kmath.js",
+        library: "kmath",
+        libraryTarget: "umd",
     },
     externals: {
         underscore: {
-            commonjs: 'underscore',
-            commonjs2: 'underscore',
-            amd: 'underscore',
-            root: '_',
+            commonjs: "underscore",
+            commonjs2: "underscore",
+            amd: "underscore",
+            root: "_",
         },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {loader: "babel-loader"},
+            },
+        ],
     },
 };
